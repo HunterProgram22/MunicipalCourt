520ed5e92c1284a9e8a1a5b88dee2a6f8f75522d
Justin Kudela <justinkudela@gmail.com>
2021-09-14 15:02:48 -0400

    Updated script to allow for user input for dates.

--- webex_download.py
+++ webex_download.py
@@ -1,15 +1,14 @@
-import requests
-import json
-from pandas import json_normalize
-from flatten_json import flatten, flatten_preserve_lists
-import pandas as pd
 import urllib
 import docx
 import datetime
+import requests
+import json
+
+from flatten_json import flatten
+from loguru import logger
+
 import docx2pdf
 from docx.oxml.shared import OxmlElement, qn
-from docx.opc.constants import RELATIONSHIP_TYPE as RT
-from docx.enum.dml import MSO_THEME_COLOR_INDEX
 from docx.enum.text import WD_ALIGN_PARAGRAPH
 
 
@@ -39,69 +38,78 @@ def add_hyperlink(paragraph, url, text):
     new_run = docx.oxml.shared.OxmlElement("w:r")
 
     # Create a new w:rPr element
-    rPr = docx.oxml.shared.OxmlElement("w:rPr")
+    rPr_element = docx.oxml.shared.OxmlElement("w:rPr")
 
     # Join all the xml elements together add add the required text to the w:r element
-    new_run.append(rPr)
+    new_run.append(rPr_element)
     new_run.text = text
     hyperlink.append(new_run)
-
     paragraph._p.append(hyperlink)
-
     return hyperlink
 
 
-BEARER_TOKEN = "YzJkOTNjZGQtYzA5ZS00ZDUxLWI0MGMtZjEwN2U5YjA2YWNhMjBlNTBkOGItMTRi_PF84_6b8180ed-7e73-4208-90f5-b67a07de84ac"
+VIDEO_PATH = "V:\\COURTROOM VIDEO PROCEEDINGS - JULY 2021 TO PRESENT\\"
+BEARER_TOKEN = "MGJkY2FlNjAtNDBiZS00YjY3LWFhNWUtYjM1ZWQ1MTVkOTNhYmE3MGY2OGMtYTQw_PF84_6b8180ed-7e73-4208-90f5-b67a07de84ac"
 
-# URL = "https://webexapis.com/v1/recordings?max=100&from=2021-08-24&to=2021-08-27&siteUrl=delawareohio.webex.com&hostEmail=jkudela@municipalcourt.org"
-URL_string = "https://webexapis.com/v1/recordings?max={max_records}&from={from_date}&to={to_date}&siteUrl={site_url}&hostEmail={email}".format(
-    max_records=100,
-    from_date="2021-09-07",
-    to_date="2021-09-08",
-    site_url="delawareohio.webex.com",
-    email="jkudela@municipalcourt.org",
-)
 
-URL = URL_string
+@logger.catch
+def main():
+    month = input("Enter month of video proceedings (i.e. 'September'):")
+    year = input(str("Enter year of video proceedings (i.e. '2021'):"))
+    start_date = input(
+        str("Enter the date (YYYY-MM-DD) of the first day of recordings to download:")
+    )
+    end_date = input(
+        str(
+            "Enter the date (YYYY-MM-DD) of the last day of recordings to download"
+            + " - download does not include the last day:"
+        )
+    )
+    URL_string = (
+        "https://webexapis.com/v1/recordings?max=100&from="
+        + "{from_date}&to={to_date}&siteUrl={site_url}&hostEmail={email}".format(
+            max_records="100",  # There is something up with max_records in the format
+            from_date=start_date,
+            to_date=end_date,
+            site_url="delawareohio.webex.com",
+            email="jkudela@municipalcourt.org",
+        )
+    )
+    logger.info(URL_string)
+    location = "Delaware City Webex"
+    headers = {
+        "Authorization": "Bearer {bearer_token}".format(bearer_token=BEARER_TOKEN)
+    }
+    response = requests.get(
+        url=URL_string,
+        headers=headers,
+        stream=True,
+    )
+    logger.info(response)
+    response_data = response.json()
+    logger.info(response_data)
+    response_items = response_data["items"]
+    mydoc = docx.Document()
+    start_date_list = start_date.split("-")
+    heading_date = (
+        start_date_list[1] + "-" + start_date_list[2] + "-" + start_date_list[0]
+    )
+    heading = mydoc.add_heading("Court Video Proceedings " + heading_date + "\n")
+    heading.alignment = WD_ALIGN_PARAGRAPH.CENTER
+    mydoc_paragraph = mydoc.add_paragraph()
+    for video_info in response_items:
+        playback_url = video_info["playbackUrl"]
+        title = video_info["topic"]
+        print(title)
+        print(playback_url)
+        run = mydoc_paragraph.add_run(title + "\n")
+        run.bold
+        run.bold = True
+        run_link = add_hyperlink(mydoc_paragraph, playback_url, "Video Link")
+        mydoc_paragraph.add_run("\n\n")
+    document_name = "Courtroom_Proceedings_" + heading_date + ".docx"
+    mydoc.save(VIDEO_PATH + month + " " + year + "\\" + document_name)
+    docx2pdf.convert(VIDEO_PATH + month + " " + year + "\\" + document_name)
 
-location = "Delaware City Webex"
-headers = {"Authorization": "Bearer {bearer_token}".format(bearer_token=BEARER_TOKEN)}
-response = requests.get(
-    url=URL,
-    headers=headers,
-    stream=True,
-)
-response_data = response.json()
-print(response_data)
-response_items = response_data["items"]
-print(response_items)
 
-mydoc = docx.Document()
-heading_date = "09-07-2021"  # str(datetime.datetime.now().strftime("%m-%d-%Y"))
-heading = mydoc.add_heading("Court Video Proceedings " + heading_date + "\n")
-heading.alignment = WD_ALIGN_PARAGRAPH.CENTER
-paragraph = mydoc.add_paragraph()
-# download_url = response_items[0]['downloadUrl']
-for video_info in response_items:
-    playback_url = video_info["playbackUrl"]
-    title = video_info["topic"]
-    print(title)
-    print(playback_url)
-    run = paragraph.add_run(title + "\n")
-    run.bold
-    run.bold = True
-    run_link = add_hyperlink(paragraph, playback_url, "Video Link")
-    paragraph.add_run("\n\n")
-
-# mydoc.save("C:\\users\\jkudela\\Desktop\\test_download.docx")
-
-mydoc.save(
-    "V:\\COURTROOM VIDEO PROCEEDINGS - JULY 2021 TO PRESENT\\September 2021\\Courtroom_Proceedings_"
-    + heading_date
-    + ".docx"
-)
-docx2pdf.convert(
-    "V:\\COURTROOM VIDEO PROCEEDINGS - JULY 2021 TO PRESENT\\September 2021\\Courtroom_Proceedings_"
-    + heading_date
-    + ".docx"
-)
+main()
